7034bdd2e541ddf944bb269d112cfadc814360de
alentek <alentek@github.com>
2019-05-15 17:39:10 +0800

    添加总例程

--- 9_ip_fifo/par/simulation/modelsim/rtl_work/ip_fifo/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ip_fifo is
-    port(
-        sys_clk         : in     vl_logic;
-        sys_rst_n       : in     vl_logic
-    );
-end ip_fifo;
